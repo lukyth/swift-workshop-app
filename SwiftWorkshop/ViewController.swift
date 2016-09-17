9fe461009f4d53fd39bcb0bd712c1b6f4c276084
Kanitkorn S <k.sujautra@gmail.com>
2016-09-17 12:58:23 +0700

    Initial Commit

---
+++ SwiftWorkshop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftWorkshop
+//
+//  Created by Kanitkorn Sujautra on 9/17/2559 BE.
+//  Copyright © 2559 Kanitkorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
